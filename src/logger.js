d4fa85cc094ddb5d7eeb70048052095246c0df53
eviltik <eviltik@gmail.com>
2022-02-21 21:39:49 +0100

    implement logger, add date, process name

--- src/logger.js
+++ src/logger.js
@@ -1,20 +1,36 @@
+
 const logger = {};
 
-logger.info = function() {
+let d = new Date();
 
-    console.log.apply( console, arguments );
+function prefix( level, args ) {
+
+    args = [...args];
+
+    args.unshift( `devsrv - ${level}:` );
+
+    // add date;
+    d = new Date();
+    args.unshift( d.toGMTString() );
+    return args;
+
+}
+
+logger.info = function info() {
+
+    console.log.apply( console, prefix( 'info', arguments ) );
 
 };
 
-logger.error = function() {
+logger.error = function error() {
 
-    console.log.apply( console, arguments );
+    console.log.apply( console, prefix( 'error', arguments ) );
 
 };
 
-logger.warn = function() {
+logger.warn = function warn() {
 
-    console.log.apply( console, arguments );
+    console.log.apply( console, prefix( 'warn', arguments ) );
 
 };
 
@@ -22,7 +38,7 @@ if ( process.env.DEBUG ) {
 
     logger.debug = function() {
 
-        console.log.apply( console, arguments );
+        console.log.apply( console, prefix( 'debug', arguments ) );
 
     };
 
